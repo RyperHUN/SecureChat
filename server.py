03de14bcf30c10db991cf0ed3065eebdf73ed4a3
Ryperhun <akormanyzsolt@gmail.com>
2018-04-25 17:21:06 +0200

    Added forward message function for server

--- server.py
+++ server.py
@@ -28,12 +28,12 @@ def get_users():
 @app.route('/user/<string:user_mail>')
 def get_user(user_mail):
     user = [user for user in users if user['mail'] == user_mail]
-    return jsonify({'user':user});
+    return jsonify(user);
 
 #For Testing:
 #curl -i -H "Content-Type: application/json" -X POST -d '{"mail":"added_test@gmail.com", "public_key":"123key"}' http://localhost:5000/register_user
 @app.route('/register_user', methods=['POST'])
-def create_task():
+def register_user():
     if not request.json :
         abort(400)
     user = {
@@ -42,7 +42,19 @@ def create_task():
         'public_key': request.json['public_key']
     }
     users.append(user)
-    return jsonify({'user': user}), 201
+    return jsonify(user), 201
+
+#curl -i -H "Content-Type: application/json" -X POST -d '{"to":"to@gmail.com", "message":"Decriptedasda"}' http://localhost:5000/forward_message
+@app.route('/forward_message', methods=['POST'])
+def forward_message():
+    if not request.json :
+        abort(400)
+    # TODO Timestamp
+    message = {
+        'to': request.json['to'],
+        'message': request.json['message']
+    }
+    return jsonify(message);
 
 if __name__ == '__main__':
     app.run(debug=True)
