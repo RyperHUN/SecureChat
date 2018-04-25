415d2fc9093c670bb8219b5ac061cc6bec88d1a3
Ryperhun <akormanyzsolt@gmail.com>
2018-04-25 16:56:21 +0200

    register user added to server

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request,abort
 
 app = Flask(__name__)
 
@@ -29,6 +29,20 @@ def get_users():
 def get_user(user_mail):
     user = [user for user in users if user['mail'] == user_mail]
     return jsonify({'user':user});
+
+#For Testing:
+#curl -i -H "Content-Type: application/json" -X POST -d '{"mail":"added_test@gmail.com", "public_key":"123key"}' http://localhost:5000/register_user
+@app.route('/register_user', methods=['POST'])
+def create_task():
+    if not request.json :
+        abort(400)
+    user = {
+        'id': users[-1]['id'] + 1,
+        'mail': request.json['mail'],
+        'public_key': request.json['public_key']
+    }
+    users.append(user)
+    return jsonify({'user': user}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
